dc4f4d1b2e8ac9aac0dc3cac2e3c10e99c60ae2f
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-06-15 13:17:57 +0900

    Todo Controller & Todo Model OK

--- app/Todo.php
+++ app/Todo.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    protected $guarded = ['id'];
+    protected $table = 'todo';
+    public static $rules = [
+        'title' => 'required',
+        'content' => 'required',
+    ];
+
+    public function scopeFlg ($query, $num) {
+        return $query->where ('flg', $num);
+    }
 }
